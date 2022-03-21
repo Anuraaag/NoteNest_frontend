fa88ea306c5a206ad067ce706edbb183a7c2050e
Anuraaag <anuraggupta10000@gmail.com>
2022-03-22 01:53:28 +0530

    made update modal responsive, fixed note overflow bug

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -12,31 +12,28 @@ const NoteItem = (props) => {
     const closeModal = useRef(null)
 
     const [noteState, setNoteState] = useState(note) //creating the stateValue. The above note variable has the note fetched from the db
-    
+
     const updateValue = (e) => {
-        setNoteState({...noteState, [e.target.name]: e.target.value})
+        setNoteState({ ...noteState, [e.target.name]: e.target.value })
         // this syntax means, "keep the note as it is" but overwrite any value that is changed.
         // So here, if title is changed, the e.target.name will be equal to "title". 
         // And "title" is already a property in the note object. So its value will be used to update our note object
     }
 
     const callUpdateNote = async () => {
-        await updateNote(noteState)        
+        await updateNote(noteState)
         closeModal.current.click()
     }
 
-    // const showAlert = useContext(AlertContext).showAlert
     return (
         <>
             <div className='col-md-4 col-sm-6 my-3'>
                 <div className="card" >
                     <div className="card-body">
-                        <div className='d-flex justify-content-between'>
-                            <h5 className="card-title">{note.title}</h5>
+                        <div className='d-flex justify-content-between flex-wrap'>
+                            <h5 style={{'word-break': 'break-word'}}>{note.title}</h5>
                             <div>
                                 <i className="fa-solid fa-pen-to-square m-1 mx-2 text-warning" data-toggle="modal" data-target={`#${note._id}`} ></i>
-
-                                {/*onClick={showAlert("hello", "success")}*/}
 
                                 <i className="fa-solid fa-trash-can m-1 mx-2 text-danger" onClick={() => { deleteNote(note._id) }}></i>
                                 {/* Wrapped the function call in an anonymous function, to prevent it from being called.
@@ -60,16 +57,16 @@ const NoteItem = (props) => {
                             </button>
                         </div>
                         <div className="modal-body">
-                            <div className="mb-3 mt-4 d-flex justify-content-between">
-                                <input type="text" className="form-control col-md-6 d-inline-block" id="title" name="title" placeholder="Note Title" value={noteState.title} onChange={updateValue} />
-                                <input type="text" className="form-control col-md-5 d-inline-block" id="tag" name="tag" placeholder="Note Tag" value={noteState.tag} onChange={updateValue} />
+                            <div className="mb-3 mt-4 d-sm-flex d-md-flex d-xs-flex justify-content-between">
+                                <input type="text" className="form-control col-md-6 col-sm-6 col-12 d-sm-inline-block d-md-inline-block d-lg-inline-block mb-sm-0 mb-md-0 mb-lg-0 mb-3" id="title" name="title" placeholder="Note Title" value={noteState.title} onChange={updateValue} />
+                                <input type="text" className="form-control col-md-5 col-sm-5 col-12  d-sm-inline-block d-md-inline-block d-lg-inline-block" id="tag" name="tag" placeholder="Note Tag" value={noteState.tag} onChange={updateValue} />
                             </div>
                             <div className="mb-3">
                                 <textarea className="form-control" id="description" name="description" rows="5" placeholder="Enter note" value={noteState.description} onChange={updateValue} ></textarea>
                             </div>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" ref={closeModal} className="btn btn-secondary" data-dismiss="modal" onClick={() => {setNoteState(note)}} >Close</button>
+                            <button type="button" ref={closeModal} className="btn btn-secondary" data-dismiss="modal" onClick={() => { setNoteState(note) }} >Close</button>
                             <button disabled={noteState.title.length < 3 || noteState.description.length < 5} type="button" className="btn btn-primary" onClick={callUpdateNote} >Save changes</button>
                         </div>
                     </div>
